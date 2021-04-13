2e7a752e96be721542a5ed642b0c061e5161be6e
TulioliAles <desmecad@hotmail.com>
2021-04-12 21:34:27 -0300

    Cria View Component ListaTarefas, View Criar Tarefa e métodos para salvar tarefa no BD

---
+++ AgendaTarefa/ViewComponents/ListaTarefasViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using AgendaTarefa.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgendaTarefa.ViewComponents
+{
+    public class ListaTarefasViewComponent : ViewComponent
+    {
+        private readonly Contexto _contexto;
+
+        public ListaTarefasViewComponent(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string data)
+        {
+            return View(await _contexto.Tarefas
+                .OrderBy(t => t.Horario).Where(t => t.Data == data).ToListAsync());
+        }
+    }
+}
